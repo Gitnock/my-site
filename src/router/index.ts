4def1d2156bdf5532980f93ebeaeace128f4e6d5
Enock Lubega <enocklubega@gmail.com>
2022-01-04 05:40:59 -0500

    cleanedUp index.vue and about.vue html and css

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Index from '../views/Index.vue';
+import About from '../views/About.vue';
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'Home',
         component: Index,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About,
     }
 ]
 
